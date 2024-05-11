99beb85cb70bb007d02cc430e1160f4e6b831f4c
itsabhishekpatel01 <itsabhishekpatel01@gmail.com>
2024-05-12 00:19:27 +0530

    variables and data types

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 212301
+var accountEmail = "abhishek@google.com"
+let accountAdress= "India"
+let accountState;
+
+/*
+prefer not to use var
+because of issue with block scope and functional scope
+*/
+
+// accountId=101
+
+console.table([accountId,accountEmail,accountAdress,accountState])
---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+2) we are using nodejs, not browser
+
+/*
+number => 2 to the power 53
+bigInt
+string => ""
+boolean => true/false
+null => stand alone value
+undefined => when variable is not assigned when declared
+*/
+
+let num = 2
+let name = "Abhishek"
+let percentage = 2.3
+let isPassed = true
+let temperature = null
+let random
+
+console.table([num,name,percentage,isPassed,temperature,random])
+
+console.log(typeof "Abhishek") //string
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
