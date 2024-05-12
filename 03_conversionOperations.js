441f0d5c0d4171de3953af1c3bbb2065aea17f89
itsabhishekpatel01 <itsabhishekpatel01@gmail.com>
2024-05-12 10:27:26 +0530

    operations

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -1,9 +1,9 @@
 let score = "33"
 let scoreInNumber = Number(score)
 
-console.log(typeof score)
-console.log (typeof(score))
-console.log(typeof scoreInNumber)
+// console.log(typeof score)
+// console.log (typeof(score))
+// console.log(typeof scoreInNumber)
 
 /*
 "123" => 123
@@ -13,7 +13,7 @@ true => 1; false => 0;
 
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 0 => false; 1 => true
 // "" => false
@@ -21,5 +21,31 @@ console.log(booleanIsLoggedIn)
 
 let age = 23
 let ageInString = String(age)
-console.log(ageInString)
-console.log(typeof ageInString)
+// console.log(ageInString)
+// console.log(typeof ageInString)
+
+//***********Operations**********************/
+let value = 5
+let negValue = -value
+// console.log(negValue);
+
+// console.log(1+2);
+// console.log(5-2);
+// console.log(1*2);
+// console.log(6%2);
+// console.log(10/3);
+
+// console.log("Hello" + " Abhishek");
+// console.log(1 + "2"); // 12
+// console.log("1" + 2); //12
+// console.log("1" + 2 + 5); //125
+// console.log(1 + 2 + "5"); // 35
+
+//Pro Tip: Never write confusing code
+// Always try to write your code as readable as possible
+
+let counter = 100
+console.log(++counter); //101
+console.log(counter--); //101
+
+
