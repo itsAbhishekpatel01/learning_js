7e5506d3117bd850d7e30bfafe5661038e831110
itsabhishekpatel01 <itsabhishekpatel01@gmail.com>
2024-05-12 10:06:36 +0530

    Conversion of data types

---
+++ 03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "33"
+let scoreInNumber = Number(score)
+
+console.log(typeof score)
+console.log (typeof(score))
+console.log(typeof scoreInNumber)
+
+/*
+"123" => 123
+"abc12" => NaN
+true => 1; false => 0;
+*/
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 0 => false; 1 => true
+// "" => false
+// "Abhishek" => true
+
+let age = 23
+let ageInString = String(age)
+console.log(ageInString)
+console.log(typeof ageInString)
